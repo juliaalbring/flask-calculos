d2c32b28245cf1ea5e7635fd883e6b4ddfd33852
juliaalbring <141272912+juliaalbring@users.noreply.github.com>
2025-03-27 14:54:32 -0300

    Update auladia25.py
    Alterações da aula do dia 27/03

--- auladia25.py
+++ auladia25.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import uuid
 
 app = Flask(__name__)
 
@@ -11,12 +12,44 @@ def samrnum():
     numero2 = dados_recebidos ['numero2']
     resultado = numero1 + numero2
     lista_calculos.append({
+        'id': str(uuid.uuid4()),
         'numero1' : numero1,
         'numero2' : numero2,
-        'resultado' : resultado 
-    })
+        'resultado' : resultado })
+    
     return {'resultado': resultado}
 
 @app.route('/calculos', methods= ['GET'])
 def calculos():
     return lista_calculos
+
+
+
+'''como podemos excluir um calculo da lista?'''
+@app.route('/deletar/<id>', methods=['DELETE'])
+def deletar_item(id):
+    global lista_calculos
+    lista_calculos = [d for d in lista_calculos if d.get('id') !=id]
+
+    return{}
+
+@app.route('/editar/<id>', methods=['PUT'])
+def editar_item(id):
+     global lista_calculos
+     dados_recebidos = request.get_json()
+     numero1 = dados_recebidos['numero1']
+     numero2 = dados_recebidos['numero2']
+
+     lista_temp = []
+     for calculo in lista_calculos:
+         if calculo['id'] == id:
+            resultado = numero1 + numero2
+            lista_calculos.append({
+                'id': str(uuid.uuid4()),
+                'numero1' : numero1,
+                'numero2' : numero2,
+                'resultado' : resultado })
+         else:
+             lista_temp.append(calculo)
+     lista_calculos = lista_temp
+     return{}
